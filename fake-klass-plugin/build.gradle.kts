a84950675c885d3c77db41b505479658e3c74505
mohaberabi <mohaberabi98@icloud.com>
2025-12-13 23:35:18 +0200

    fix

--- fake-klass-plugin/build.gradle.kts
+++ fake-klass-plugin/build.gradle.kts
@@ -12,7 +12,10 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
 }
-
+repositories {
+    google()
+    mavenCentral()
+}
 kotlin {
     compilerOptions {
         jvmTarget.set(JvmTarget.JVM_17)
