9d97ade938ce1a57e095c1a56a36944f4e38b17f
mohaberabi <mohaberabi98@icloud.com>
2025-12-13 23:09:24 +0200

    fix

--- fake-klass/src/main/kotlin/fakeklass/processor/utils/ArgsUtils.kt
+++ fake-klass/src/main/kotlin/fakeklass/processor/utils/ArgsUtils.kt
@@ -2,7 +2,6 @@ package fakeklass.processor.utils
 
 import fakeklass.processor.OUTPUT_PACKAGE_ARG
 import fakeklass.processor.PACKAGES_ARG
-import fakeklass.processor.SOURCE_SET_ARG
 
 
 internal fun Map<String, String>.fakeKlassPackages() = (this[PACKAGES_ARG] ?: "").split(".")
